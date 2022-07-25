7066a9d844c06633de0690a9182266038583cd08
Jared Shelby <jared.shelby@yale.edu>
2022-07-25 16:10:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    // g is a modifier that specifies to replace globally (not just the first value)
+    return string.replace( /[^A-Za-z0-9 '-]/g, "");
+  }
+
+  static titleize(string) {
+    let exclusions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let arr = string.split(" ");
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i === 0) {
+        newArr.push(this.capitalize(arr[i]));
+      } else if (exclusions.includes(arr[i])) {
+        newArr.push(arr[i]);
+      } else {
+        newArr.push(this.capitalize(arr[i]));
+      }
+    }
+    return newArr.join(" ");
+  }
 }
